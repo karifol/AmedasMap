2425fb11d27df6462945908904ab657e80bd7233
karifol <karigolhorinouchi@gmail.com>
2024-06-11 23:06:52 +0900

    Initial Commit

---
+++ AmedasMap/AmedasMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmedasMapApp.swift
+//  AmedasMap
+//
+//  Created by 堀ノ内海斗 on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct AmedasMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmedasMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AmedasMap
+//
+//  Created by 堀ノ内海斗 on 2024/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
